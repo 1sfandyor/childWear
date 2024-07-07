0e07395d44fc6e5f2b0d0ba5a1dcf646fc5ac0f0
1sfandyor <isfandiyor06112000@gmail.com>
2024-07-07 22:25:21 +0500

    Update

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,11 +1,49 @@
+import { useEffect, useState } from "react";
+import { Get } from "../../Hooks/GET";
 import { HeroBanner } from "../../components/HeroBanner/HeroBanner"
+import { HorizontalCategory } from "../../components/HorizontalCategory/HorizontalCategory"
 import { MainCategory } from "../../components/MainCategory/MainCategory"
 
 const Home = () => {
+
+    const API = 'https://api.jsonbin.io/v3/b/66898de7e41b4d34e40e0fc5';
+    const [outfitCategory, setOutfitCategory] = useState([]);
+    const [newCollection, setNewCollection] = useState([]);
+    const [topBestSeller, setTopBestSeller] = useState([]);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            const responseData = await Get(API);
+            const outfitCategoryTemp = [];
+            const newCollectionTemp = [];
+            const topBestSellerTemp = [];
+
+            responseData?.record?.Category?.forEach(item => {
+                if (item.name === 'Category') {
+                    outfitCategoryTemp.push(item.info);
+                }else if (item.name === 'newCollectionData') {
+                    newCollectionTemp.push(item.info);
+                }else if (item.name === 'TopBestSeller') {
+                    topBestSellerTemp.push(item.info);
+                }
+            });
+
+            setOutfitCategory(outfitCategoryTemp);
+            setNewCollection(newCollectionTemp);
+            setTopBestSeller(topBestSellerTemp);
+        };
+
+        fetchData();
+    }, []);
+
+    console.log(topBestSeller);
+
     return (
         <>
             <HeroBanner/>
-            <MainCategory/>
+            <MainCategory outfitCategory={outfitCategory}/>
+            <HorizontalCategory newCollection={newCollection}/>
+            <HorizontalCategory topBestSeller={topBestSeller}/>
         </>
     )
 }
--- src/components/MainCategory/MainCategory.jsx
+++ src/components/MainCategory/MainCategory.jsx
@@ -1,57 +1,25 @@
-import { useEffect, useState } from "react";
-import { Get } from "../../Hooks/GET";
+import { Fragment} from "react";
 import { ProductCard } from "../ProductCard/ProductCard";
 
-export const MainCategory = () => {  
-  const API = 'https://api.jsonbin.io/v3/b/66898de7e41b4d34e40e0fc5';
-  const [data, setData] = useState([]);
-  const [outfitCategory, setOutfitCategory] = useState([]);
-  const [newCollection, setNewCollection] = useState([]);
-  const [topBestSeller, setTopBestSeller] = useState([]);
-
-  useEffect(() => {
-    const fetchData = async () => {
-      const responseData = await Get(API, setData);
-      const outfitCategoryTemp = [];
-      const newCollectionTemp = [];
-      const topBestSellerTemp = [];
-
-      responseData?.record?.Category?.forEach(item => {
-        if (item.name === 'Category') {
-          outfitCategoryTemp.push(item.info);
-        } else if (item.name === 'newCollectionData') {
-          newCollectionTemp.push(item.info);
-        } else if (item.name === 'TopBestSeller') {
-          topBestSellerTemp.push(item.info);
-        }
-      });
-
-      setOutfitCategory(outfitCategoryTemp);
-      setNewCollection(newCollectionTemp);
-      setTopBestSeller(topBestSellerTemp);
-    };
-    fetchData();
-  }, []);
+export const MainCategory = ({outfitCategory}) => {  
 
   return (
     <div className="grid grid-cols-2 mb-[119px]">
       {
         outfitCategory.map(item => item.map(outfit => {
           return(
-            <ProductCard className={''}
+            <Fragment key={outfit.id}>
+              <ProductCard className={''}
                           size={outfit.size}
                           count={outfit.count}
                           heading={outfit.title}
                           price={outfit.price}
                           img={outfit.image}
             />
+            </Fragment>
           )
         }))
       }
     </div>
   );
 };
-
-{/* 
-
-*/}
